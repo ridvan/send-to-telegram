e50c341508ff8bd4b4d357d986e76c4cb8b0594f
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-02-04 01:10:14 +0300

    fix(logs): en locale for timestamp converter

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,6 @@
 // timestampToReadableDate function was written by GPT-3.5, I updated it afterward
 export const timestampToReadableDate = function (unixTimestamp) {
     const date = new Date(unixTimestamp);
-    return `${date.toLocaleString('default', { month: 'short' })} 
+    return `${date.toLocaleString('en-US', { month: 'short' })} 
     ${date.getDate()}, ${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`;
 };
